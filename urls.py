2f6cfffb83fde185f2914b4e7356952836572e10
rduenasf (Rodrigo Dueñas) <rduenasf@gmail.com>
2011-11-22 17:24:05 -0300

    Fixed accounts urls bug

--- urls.py
+++ urls.py
@@ -8,18 +8,16 @@ admin.autodiscover()
 from elections.views import ElectionDetailView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'candidator.views.home', name='home'),
-    # url(r'^candidator/', include('candidator.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     (r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
     (r'^profiles/', include('profiles.urls')),
+
+    # django-registration urls, maps common registration urls to the ones in django.contrib.auth
+    url(r'^accounts/', include('registration.urls')),
+
 
     url(r'^', include('elections.urls')),
 
@@ -27,8 +25,6 @@ urlpatterns = patterns('',
     url(r'^(?P<my_user>[a-zA-Z0-9-]+)/(?P<election_slug>[a-zA-Z0-9-]+)/medianaranja/$', 'candidator.elections.views.medianaranja1',name='medianaranja1'),
     url(r'^(?P<user>[a-zA-Z0-9-]+)/(?P<election>[a-zA-Z0-9-]+)/medianaranja/$', 'candidator.elections.views.medianaranja2',name='medianaranja2'),
 
-    # django-registration urls, maps common registration urls to the ones in django.contrib.auth
-    (r'^accounts/', include('registration.urls')),
 
     (r'^$', direct_to_template, {'template': 'index.html'}),
 
