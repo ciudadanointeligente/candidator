81e6de40a91913e9b63fba62ffc7c9cd33e115a3
Nicolas Malbran <nmalbran@gmail.com>
2011-12-02 18:01:10 -0300

    Split views.py in folder package

---
+++ elections/views/__init__.py
@@ -0,0 +1,3 @@
+from views import *
+
+from election import *
--- elections/views/views.py
+++ elections/views/views.py
@@ -15,11 +15,11 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_http_methods
 from django.views.generic import CreateView, DetailView, UpdateView
 
-from forms import CandidateForm, CandidateUpdateForm, CategoryForm, ElectionForm,\
+from elections.forms import CandidateForm, CandidateUpdateForm, CategoryForm, ElectionForm,\
                   CandidatePersonalInformationForm, CandidatePersonalInformationFormset,\
                   CandidateLinkFormset, ElectionUpdateForm, CategoryUpdateForm,\
                   PersonalDataForm, BackgroundCategoryForm, BackgroundForm, QuestionForm
-from models import Election, Candidate, Answer, PersonalInformation,\
+from elections.models import Election, Candidate, Answer, PersonalInformation,\
                    Link, Category, Question, PersonalData,\
                    BackgroundCategory, Background
 
@@ -60,20 +60,6 @@ class CandidateUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('candidate_update', kwargs={'slug': self.kwargs['slug'], 'election_slug': self.object.election.slug})
-
-
-
-class ElectionUpdateView(UpdateView):
-    model = Election
-    form_class = ElectionUpdateForm
-
-    def get_success_url(self):
-        return reverse('election_update', kwargs={'slug': self.object.slug})
-
-    @method_decorator(login_required)
-    def dispatch(self, request, *args, **kwargs):
-        election = get_object_or_404(Election, slug=kwargs['slug'], owner=request.user)
-        return super(ElectionUpdateView, self).dispatch(request, *args, **kwargs)
 
 
 class CandidateCreateView(CreateView):
@@ -127,41 +113,6 @@ class CandidateCreateView(CreateView):
                 return super(CandidateCreateView, self).form_invalid(form)
 
             return super(CandidateCreateView, self).form_valid(form)
-
-
-# Election views
-class ElectionDetailView(DetailView):
-    model = Election
-
-    def get_queryset(self):
-        if self.kwargs.has_key('username') and self.kwargs.has_key('slug'):
-            return self.model.objects.filter(owner__username=self.kwargs['username'],
-                                             slug=self.kwargs['slug'])
-        return super(ElectionDetailView, self).get_queryset()
-
-
-class ElectionCreateView(CreateView):
-    model = Election
-    form_class = ElectionForm
-
-    @method_decorator(login_required)
-    def dispatch(self, request, *args, **kwargs):
-        return super(ElectionCreateView, self).dispatch(request, *args, **kwargs)
-
-    def get_success_url(self):
-        return reverse('candidate_create', kwargs={'election_slug': self.object.slug})
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.owner = self.request.user
-        try:
-            self.object.full_clean()
-        except ValidationError:
-            from django.forms.util import ErrorList
-            form._errors["slug"] = ErrorList([u"Ya tienes una eleccion con ese slug."])
-            return super(ElectionCreateView, self).form_invalid(form)
-
-        return super(ElectionCreateView, self).form_valid(form)
 
 
 class CategoryCreateView(CreateView):
