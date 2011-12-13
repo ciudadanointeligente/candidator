7bca08efbdb5a14ca22cb7bf006adc1feccc9e47
Nicolas Malbran <nmalbran@gmail.com>
2011-12-13 16:39:16 -0300

    Splited forms in package folder

---
+++ elections/forms/candidate_form.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.forms import formsets
+from django.forms.formsets import formset_factory
+from elections.models import Candidate, PersonalInformation, Link
+
+class CandidateForm(forms.ModelForm):
+    class Meta:
+        model = Candidate
+        exclude = ('election', 'answers', 'personal_data', 'background')
+    class Media:
+        js = ('jquery.slug.js', 'jquery.formset.js', )
+
+
+class CandidateUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Candidate
+        exclude = ('slug', 'election', 'answers', 'personal_data', 'background')
+    class Media:
+        js = ('jquery.formset.js', )
+
+
+class CandidatePersonalInformationForm(forms.ModelForm):
+    class Meta:
+        model = PersonalInformation
+        exclude = ('candidate', )
+
+CandidatePersonalInformationFormset = formsets.formset_factory(CandidatePersonalInformationForm, extra=1)
+
+
+class CandidateLinkForm(forms.ModelForm):
+    class Meta:
+        model = Link
+        exclude = ('candidate', )
+
+CandidateLinkFormset = formsets.formset_factory(CandidateLinkForm, extra=1)
