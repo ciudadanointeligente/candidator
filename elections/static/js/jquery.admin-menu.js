fae0a5005d98305a509d8c31368763c3990e063b
Pedro Burón <pedro@witoi.com>
2012-01-10 12:29:28 -0300

    added highlight to errors in form

--- elections/static/js/jquery.admin-menu.js
+++ elections/static/js/jquery.admin-menu.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     if ($(".candidate-edit-menu")) {
+        $(window).resize(function() {
+        $(".candidate-edit-menu").height($(".fondo_wizard").height());
+        });
         $(".candidate-edit-menu").height($(".fondo_wizard").height());
     }
-});
+    $('.errorlist li').effect('highlight', 3000);
+})
