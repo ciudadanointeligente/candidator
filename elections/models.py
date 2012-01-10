c532cc6a84131924c5e6e021b3bb505484344a49
Pedro Burón <pedro@witoi.com>
2012-01-10 12:07:35 -0300

    added finished wizar

--- elections/models.py
+++ elections/models.py
@@ -35,7 +35,10 @@ class Election(models.Model):
 
     def __unicode__(self):
         return u"%s" % self.name
-
+    
+    @models.permalink
+    def get_absolute_url(self):
+        return ('election_detail', None, {'username': self.owner.username, 'slug': self.slug})
 
 class Candidate(models.Model):
     name = models.CharField(max_length=255, verbose_name="Nombre:")
